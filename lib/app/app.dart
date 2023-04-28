6123f5d68b78e039009f5095b674865d94999a0d
Ahmed Elsankary <elsankary99@gmail.com>
2023-04-28 17:03:41 +0200

    first commit

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp._internal();
+  static const MyApp _instance = MyApp._internal();
+  factory MyApp() => _instance;
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:tutapp/app/app.dart';
+
+void main() {
+  runApp(MyApp());
+}
