7f62adef65c4fc87288cd8a9972fab50aa2408bc
Ahmed Elsankary <elsankary99@gmail.com>
2023-05-06 22:41:07 +0300

    login view part 2

--- lib/presentation/login/view/login_view.dart
+++ lib/presentation/login/view/login_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tutapp/presentation/login/view_model/login_view_model.dart';
 import 'package:tutapp/presentation/resources/assets_manager.dart';
+import 'package:tutapp/presentation/resources/string_manager.dart';
 import 'package:tutapp/presentation/resources/value_manager.dart';
 
 class LoginView extends StatefulWidget {
@@ -53,16 +54,44 @@ class _LoginViewState extends State<LoginView> {
                 Padding(
                   padding:
                       const EdgeInsets.symmetric(horizontal: AppPadding.p28),
-                  child: StreamBuilder(
+                  child: StreamBuilder<bool>(
                     stream: _viewModel.outIsUserNameValid,
                     builder: (context, snapshot) {
                       return TextFormField(
                         keyboardType: TextInputType.emailAddress,
                         controller: _userNameController,
+                        decoration: InputDecoration(
+                            hintText: AppStrings.username,
+                            labelText: AppStrings.username,
+                            errorText: (snapshot.data ?? true)
+                                ? null
+                                : AppStrings.usernameError),
                       );
                     },
                   ),
-                )
+                ),
+                const SizedBox(
+                  height: AppSize.s28,
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: AppPadding.p28),
+                  child: StreamBuilder<bool>(
+                    stream: _viewModel.outIsPasswordValid,
+                    builder: (context, snapshot) {
+                      return TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        controller: _userPasswordController,
+                        decoration: InputDecoration(
+                            hintText: AppStrings.password,
+                            labelText: AppStrings.password,
+                            errorText: (snapshot.data ?? true)
+                                ? null
+                                : AppStrings.passwordError),
+                      );
+                    },
+                  ),
+                ),
               ],
             ),
           ),
