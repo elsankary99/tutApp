da6fa40c1e529b268152e49d04b8224dd03c9ade
Ahmed Elsankary <elsankary99@gmail.com>
2023-05-18 20:27:20 +0300

    adding uI for register view

--- lib/presentation/register/view/register_view.dart
+++ lib/presentation/register/view/register_view.dart
@@ -1,6 +1,14 @@
+import 'package:country_code_picker/country_code_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:tutapp/app/constants.dart';
 import 'package:tutapp/app/di.dart';
+import 'package:tutapp/presentation/common/state_renderer/state_renderer_impl.dart';
 import 'package:tutapp/presentation/register/view_model/register_view_model.dart';
+import 'package:tutapp/presentation/resources/assets_manager.dart';
+import 'package:tutapp/presentation/resources/color_manager.dart';
+import 'package:tutapp/presentation/resources/string_manager.dart';
+
+import '../../resources/value_manager.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -47,7 +55,173 @@ class _RegisterViewState extends State<RegisterView> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: ColorManager.white,
+      appBar: AppBar(
+        elevation: AppSize.s0,
+        backgroundColor: ColorManager.white,
+        iconTheme: IconThemeData(color: ColorManager.primary),
+      ),
+      body: StreamBuilder<FlowState>(
+        stream: _viewModel.outputState,
+        builder: (context, snapshot) {
+          return snapshot.data?.getScreenWidget(context, _getContentWidget(),
+                  () {
+                _viewModel.register();
+              }) ??
+              _getContentWidget();
+        },
+      ),
+    );
+  }
+
+  Widget _getContentWidget() {
+    return Container(
+        padding: const EdgeInsets.only(top: AppPadding.p28),
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const Center(
+                    child: Image(image: AssetImage(ImageAssets.splashLogo))),
+                const SizedBox(
+                  height: AppSize.s28,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: AppPadding.p28, right: AppPadding.p28),
+                  child: StreamBuilder<String?>(
+                      stream: _viewModel.outputErrorUserName,
+                      builder: (context, snapshot) {
+                        return TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _userNameEditingController,
+                          decoration: InputDecoration(
+                              hintText: AppStrings.username,
+                              labelText: AppStrings.username,
+                              errorText: snapshot.data),
+                        );
+                      }),
+                ),
+                const SizedBox(
+                  height: AppSize.s18,
+                ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: AppPadding.p28, right: AppPadding.p28),
+                    child: Row(
+                      children: [
+                        Expanded(
+                            flex: 1,
+                            child: CountryCodePicker(
+                              onChanged: (country) {
+                                // update view model with code
+                                _viewModel.setCountryCode(
+                                    country.code ?? Constants.token);
+                              },
+                              initialSelection: '+02',
+                              favorite: const ['+39', 'FR', "+966"],
+                              // optional. Shows only country name and flag
+                              showCountryOnly: true,
+                              hideMainText: true,
+                              // optional. Shows only country name and flag when popup is closed.
+                              showOnlyCountryWhenClosed: true,
+                            )),
+                        Expanded(
+                            flex: 4,
+                            child: StreamBuilder<String?>(
+                                stream: _viewModel.outputErrorMobileNumber,
+                                builder: (context, snapshot) {
+                                  return TextFormField(
+                                    keyboardType: TextInputType.phone,
+                                    controller: _mobileNumberEditingController,
+                                    decoration: InputDecoration(
+                                        hintText: AppStrings.mobileNumber,
+                                        labelText: AppStrings.mobileNumber,
+                                        errorText: snapshot.data),
+                                  );
+                                }))
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: AppSize.s18,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: AppPadding.p28, right: AppPadding.p28),
+                  child: StreamBuilder<String?>(
+                      stream: _viewModel.outputErrorEmail,
+                      builder: (context, snapshot) {
+                        return TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _emailEditingController,
+                          decoration: InputDecoration(
+                              hintText: AppStrings.emailHint,
+                              labelText: AppStrings.emailHint,
+                              errorText: snapshot.data),
+                        );
+                      }),
+                ),
+                const SizedBox(
+                  height: AppSize.s18,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: AppPadding.p28, right: AppPadding.p28),
+                  child: StreamBuilder<String?>(
+                      stream: _viewModel.outputErrorPassword,
+                      builder: (context, snapshot) {
+                        return TextFormField(
+                          keyboardType: TextInputType.visiblePassword,
+                          controller: _passwordEditingController,
+                          decoration: InputDecoration(
+                              hintText: AppStrings.password,
+                              labelText: AppStrings.password,
+                              errorText: snapshot.data),
+                        );
+                      }),
+                ),
+                const SizedBox(
+                  height: AppSize.s18,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: AppPadding.p28, right: AppPadding.p28),
+                  child: StreamBuilder<bool>(
+                      stream: _viewModel.outputAreAllInputsValid,
+                      builder: (context, snapshot) {
+                        return SizedBox(
+                          width: double.infinity,
+                          height: AppSize.s40,
+                          child: ElevatedButton(
+                              onPressed: (snapshot.data ?? false)
+                                  ? () {
+                                      _viewModel.register();
+                                    }
+                                  : null,
+                              child: const Text(AppStrings.register)),
+                        );
+                      }),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: AppPadding.p18,
+                      left: AppPadding.p28,
+                      right: AppPadding.p28),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(AppStrings.alreadyHaveAccount,
+                        style: Theme.of(context).textTheme.titleMedium),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 
   @override
--- lib/presentation/resources/string_manager.dart
+++ lib/presentation/resources/string_manager.dart
@@ -33,4 +33,7 @@ class AppStrings {
   static const String emailValid = "Email format is wrong";
   static const String mobileNumberValid = "Mobile Should ba at lest 10 digits";
   static const String passwordValid = "Password Should ba at lest 6 chars";
+  static const String alreadyHaveAccount = "Already have an account ? Login";
+  static const register = "Register";
+  static const mobileNumber = "Mobile Number";
 }
